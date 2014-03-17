3783eb5cb89c63d5e13ed59f604e25e802d1a1b7
Eric Eells <eeells@nvidia.com>
2014-03-17 15:12:48 -0700

    arm: tegra: tn8: 19x12 AUO backlight pwm change
    This changes the backlight PWM frequency from
    1KHz to 25KHz.
    
    Bug 1452437
    
    Change-Id: I48faf0e7b5529031d503eb50b5c224d86640a1e5
    Signed-off-by: Eric Eells <eeells@nvidia.com>
    Reviewed-on: http://git-master/r/397334
    (cherry picked from commit 27b17ce6cf8fac54125a0917ace375a96a93890d)
    Reviewed-on: http://git-master/r/401698
    Reviewed-by: Thomas Cherry <tcherry@nvidia.com>

--- arch/arm/mach-tegra/panel-a-1200-1920-8-0.c
+++ arch/arm/mach-tegra/panel-a-1200-1920-8-0.c
@@ -518,7 +518,7 @@ static struct platform_pwm_backlight_data dsi_a_1200_1920_8_0_bl_data = {
 	.pwm_id		= 1,
 	.max_brightness	= 255,
 	.dft_brightness	= 153,
-	.pwm_period_ns	= 1000000,
+	.pwm_period_ns	= 40161,
 	.pwm_gpio	= TEGRA_GPIO_INVALID,
 	.notify		= dsi_a_1200_1920_8_0_bl_notify,
 	/* Only toggle backlight on fb blank notifications for disp1 */
