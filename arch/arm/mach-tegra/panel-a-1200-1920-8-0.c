7e8923f44f2a702c5e432021b030077164c4067e
Santosh Katvate <skatvate@nvidia.com>
2014-04-22 20:40:20 +0530

    arm: tegra: tn8: Wait 50ms before panel reset
    - RST should be low 50ms(min.) after DSI_A_D0_P disabled (after LP state)
    
    Bug 1474583
    
    Change-Id: I88e2f2387432d43296b6c9540719f4392ecb9b6e
    Signed-off-by: Santosh Katvate <skatvate@nvidia.com>
    Reviewed-on: http://git-master/r/399783
    Reviewed-by: Animesh Kishore <ankishore@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Mitch Luban <mluban@nvidia.com>

--- arch/arm/mach-tegra/panel-a-1200-1920-8-0.c
+++ arch/arm/mach-tegra/panel-a-1200-1920-8-0.c
@@ -287,8 +287,12 @@ fail:
 
 static int dsi_a_1200_1920_8_0_disable(void)
 {
-	if (gpio_is_valid(dsi_a_1200_1920_8_0_pdata.dsi_panel_rst_gpio))
+	if (gpio_is_valid(dsi_a_1200_1920_8_0_pdata.dsi_panel_rst_gpio)) {
+		/* Wait for 50ms before triggering panel reset */
+		msleep(50);
 		gpio_set_value(dsi_a_1200_1920_8_0_pdata.dsi_panel_rst_gpio, 0);
+	}
+
 	msleep(120);
 
 	if (vdd_lcd_bl_en)
