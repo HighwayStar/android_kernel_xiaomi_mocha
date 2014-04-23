d1df0c558fa61ac9b30687e935f4fae50743a504
Michael Frydrych <mfrydrych@nvidia.com>
2014-04-23 09:15:07 +0300

    video: tegra: blank dc windows during dc release
    Window owned by a process should be blanked when the process
    releases dc or any individual window it has owned. The buffers
    lastly displayed on those windows will be unpinned.
    
    bug 1490686
    bug 1467689
    
    Change-Id: I3b3614a99b9598b7d432f08c4a95d8050a4ef99e
    Signed-off-by: Michael Frydrych <mfrydrych@nvidia.com>
    Reviewed-on: http://git-master/r/400894
    (cherry picked from commit 73896aaf41ac24c5d5b31cdbcf8aff4f8f659a3e)
    Reviewed-on: http://git-master/r/403703
    Reviewed-by: David Dastous St Hilaire <ddastoussthi@nvidia.com>
    Tested-by: David Dastous St Hilaire <ddastoussthi@nvidia.com>
    Reviewed-by: Chao Xu <cxu@nvidia.com>
    Reviewed-by: Thomas Cherry <tcherry@nvidia.com>
    Reviewed-by: Jon Mayo <jmayo@nvidia.com>

--- arch/arm/mach-tegra/include/mach/dc.h
+++ arch/arm/mach-tegra/include/mach/dc.h
@@ -794,7 +794,7 @@ bool tegra_dc_hpd(struct tegra_dc *dc);
 
 void tegra_dc_get_fbvblank(struct tegra_dc *dc, struct fb_vblank *vblank);
 int tegra_dc_wait_for_vsync(struct tegra_dc *dc);
-void tegra_dc_blank(struct tegra_dc *dc);
+void tegra_dc_blank(struct tegra_dc *dc, unsigned windows);
 
 void tegra_dc_enable(struct tegra_dc *dc);
 void tegra_dc_disable(struct tegra_dc *dc);
