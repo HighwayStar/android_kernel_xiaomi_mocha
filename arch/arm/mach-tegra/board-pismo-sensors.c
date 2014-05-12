748d50906ae638acb0f4026602d6d5415430e6d2
Sri Krishna chowdary <schowdary@nvidia.com>
2014-05-12 14:35:21 +0530

    ARM: tegra: update copyright to GPLv2
    Bug 14095051
    
    Change-Id: I6600ffc8646b07fad6e576d194b3930e808d3c6d
    Signed-off-by: Sri Krishna chowdary <schowdary@nvidia.com>
    Reviewed-on: http://git-master/r/408086
    (cherry picked from commit 52ddb17db478dc3bec040a3fa7482acc99e4b034)
    Reviewed-on: http://git-master/r/409222
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User

--- arch/arm/mach-tegra/board-pismo-sensors.c
+++ arch/arm/mach-tegra/board-pismo-sensors.c
@@ -3,21 +3,6 @@
  *
  * Copyright (c) 2012-2014 NVIDIA CORPORATION, All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *
- * Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution.
- *
- * Neither the name of NVIDIA CORPORATION nor the names of its contributors
- * may be used to endorse or promote products derived from this software
- * without specific prior written permission.
- *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
  * may be copied, distributed, and modified under those terms.
