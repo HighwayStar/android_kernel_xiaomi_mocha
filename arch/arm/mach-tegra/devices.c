788259e784f0d3cbc0b146a336c4730b9a067105
R Raj Kumar <rrajk@nvidia.com>
2014-06-04 16:02:56 +0530

    arm: tegra: enable SMMU for SDMMC4 device
    Enabled SMMU for SDMMC4 (eMMC) device
    
    Bug 1374895
    
    Change-Id: I88d8ad913c2763599353d99e9034598fa7f76441
    Signed-off-by: R Raj Kumar <rrajk@nvidia.com>
    Reviewed-on: http://git-master/r/418884
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Venu Byravarasu <vbyravarasu@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- arch/arm/mach-tegra/devices.c
+++ arch/arm/mach-tegra/devices.c
@@ -1873,6 +1873,7 @@ struct swgid_fixup tegra_swgid_fixup_t124[] = {
 	{ .name = "nvavp",	.swgids = SWGID(AVPC) | SWGID(A9AVP), },
 	{ .name = "sdhci-tegra.1",	.swgids = SWGID(SDMMC2A) },
 	{ .name = "sdhci-tegra.2",	.swgids = SWGID(SDMMC3A) },
+	{ .name = "sdhci-tegra.3",	.swgids = SWGID(SDMMC4A) },
 	{ .name = "serial8250",	.swgids = SWGID(PPCS), },
 	{ .name = "serial-tegra",	.swgids = SWGID(PPCS), },
 	{ .name = "dtv",	.swgids = SWGID(PPCS), },
