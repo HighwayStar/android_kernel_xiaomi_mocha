ae8a9252ff4e70c2533523d7b490e9e1214412a6
Preetham Chandru R <pchandru@nvidia.com>
2014-06-12 14:37:41 +0530

    arm: tegra: Don't place CSIC/D IOs in DPD mode
    According to TRM placing CSIC/D IOs in DPD mode
    is no longer available
    
    Bug 200010066
    
    Change-Id: Ic389dba406b06a7c821fa8399ae1854f3ebcac98
    Signed-off-by: Preetham Chandru R <pchandru@nvidia.com>
    Reviewed-on: http://git-master/r/422677
    (cherry picked from commit 6708ac77f5f81ab3eca61c568e8d5a3ad73392f9)
    Reviewed-on: http://git-master/r/423755
    (cherry picked from commit 3cc39667b932a9ccf3d73f00c1e4ed5583dac4c9)
    Reviewed-on: http://git-master/r/553321
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: David Schalig <dschalig@nvidia.com>
    Reviewed-by: Robert Shih <rshih@nvidia.com>
    Tested-by: Robert Shih <rshih@nvidia.com>
    Reviewed-by: Mitch Luban <mluban@nvidia.com>

--- arch/arm/mach-tegra/board-ardbeg-sensors.c
+++ arch/arm/mach-tegra/board-ardbeg-sensors.c
@@ -248,18 +248,6 @@ static struct tegra_io_dpd csib_io = {
 	.io_dpd_bit		= 1,
 };
 
-static struct tegra_io_dpd csic_io = {
-	.name			= "CSIC",
-	.io_dpd_reg_index	= 1,
-	.io_dpd_bit		= 10,
-};
-
-static struct tegra_io_dpd csid_io = {
-	.name			= "CSID",
-	.io_dpd_reg_index	= 1,
-	.io_dpd_bit		= 11,
-};
-
 static struct tegra_io_dpd csie_io = {
 	.name			= "CSIE",
 	.io_dpd_reg_index	= 1,
@@ -846,10 +834,6 @@ static int ardbeg_ov5693_front_power_on(struct ov5693_power_rail *pw)
 	if (unlikely(WARN_ON(!pw || !pw->dovdd || !pw->avdd)))
 		return -EFAULT;
 
-	/* disable CSIC/D IOs DPD mode to turn on camera for ardbeg */
-	tegra_io_dpd_disable(&csic_io);
-	tegra_io_dpd_disable(&csid_io);
-
 	if (ardbeg_get_extra_regulators())
 		goto ov5693_front_poweron_fail;
 
@@ -888,9 +872,6 @@ ov5693_front_avdd_fail:
 	gpio_set_value(CAM_RSTN, 0);
 
 ov5693_front_poweron_fail:
-	/* put CSIC/D IOs into DPD mode to save additional power for ardbeg */
-	tegra_io_dpd_enable(&csic_io);
-	tegra_io_dpd_enable(&csid_io);
 	pr_err("%s FAILED\n", __func__);
 	return -ENODEV;
 }
@@ -898,11 +879,6 @@ ov5693_front_poweron_fail:
 static int ardbeg_ov5693_front_power_off(struct ov5693_power_rail *pw)
 {
 	if (unlikely(WARN_ON(!pw || !pw->dovdd || !pw->avdd))) {
-		/* put CSIC/D IOs into DPD mode to
-		 * save additional power for ardbeg
-		 */
-		tegra_io_dpd_enable(&csic_io);
-		tegra_io_dpd_enable(&csid_io);
 		return -EFAULT;
 	}
 
@@ -915,9 +891,6 @@ static int ardbeg_ov5693_front_power_off(struct ov5693_power_rail *pw)
 	regulator_disable(pw->dovdd);
 	regulator_disable(pw->avdd);
 
-	/* put CSIC/D IOs into DPD mode to save additional power for ardbeg */
-	tegra_io_dpd_enable(&csic_io);
-	tegra_io_dpd_enable(&csid_io);
 	return 0;
 }
 
@@ -1112,8 +1085,6 @@ static int ardbeg_camera_init(void)
 	 */
 	tegra_io_dpd_enable(&csia_io);
 	tegra_io_dpd_enable(&csib_io);
-	tegra_io_dpd_enable(&csic_io);
-	tegra_io_dpd_enable(&csid_io);
 	tegra_io_dpd_enable(&csie_io);
 
 	platform_device_add_data(&ardbeg_camera_generic,
