54f99ba90462c1ffaa763eb4bd6d3b610a812e83
Dmitry Shmidt <dimitrysh@google.com>
2014-06-24 13:19:46 -0700

    net: wireless: Add NL80211_FLAG_NEED_WIPHY flag to vendor command
    Change-Id: I52ee3bc8a422c2a4c57cccccccd6ba3e721b4c01
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- net/wireless/nl80211.c
+++ net/wireless/nl80211.c
@@ -9216,7 +9216,8 @@ static struct genl_ops nl80211_ops[] = {
 		.doit = nl80211_vendor_cmd,
 		.policy = nl80211_policy,
 		.flags = GENL_ADMIN_PERM,
-		.internal_flags = NL80211_FLAG_NEED_RTNL,
+		.internal_flags = NL80211_FLAG_NEED_WIPHY |
+				  NL80211_FLAG_NEED_RTNL,
 	},
 };
 
