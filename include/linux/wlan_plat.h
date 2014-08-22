4fea72ff09e27386f9e629a44e079dfa6a51ace0
Dmitry Shmidt <dimitrysh@google.com>
2014-08-22 14:40:18 -0700

    Add flags parameter to get_country_code template
    Change-Id: Ic3f173db144a301ea104f544fc8ec723241c1d59
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- include/linux/wlan_plat.h
+++ include/linux/wlan_plat.h
@@ -15,6 +15,8 @@
  */
 #ifndef _LINUX_WLAN_PLAT_H_
 #define _LINUX_WLAN_PLAT_H_
+#define WLAN_PLAT_NODFS_FLAG   0x01
+
 
 #include <linux/sysedp.h>
 
@@ -24,7 +26,7 @@ struct wifi_platform_data {
 	int (*set_carddetect)(int val);
 	void *(*mem_prealloc)(int section, unsigned long size);
 	int (*get_mac_addr)(unsigned char *buf);
-	void *(*get_country_code)(char *ccode);
+	void *(*get_country_code)(char *ccode, u32 flags);
 	struct sysedp_consumer *sysedpc;
 };
 
