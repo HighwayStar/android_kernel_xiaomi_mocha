7f7f77e05fc6f2d646188c94294935ec7030bc9c
Krishna Reddy <vdumpa@nvidia.com>
2014-08-28 15:06:27 -0700

    arm: tegra: retry on vpr update config failure
    sleep and retry on vpr update config on failure.
    Bug 1517584
    
    Change-Id: Ib3320037aba651de56d2f9f9efcb2cfb9bbdca0a
    Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-on: http://git-master/r/496627
    (cherry picked from commit 4a84c936049e5ce68db5d5eb6f1d788b690511c5)
    Signed-off-by: Vandana Salve <vsalve@nvidia.com>
    Reviewed-on: http://git-master/r/602754
    Reviewed-by: Dhiren Parmar <dparmar@nvidia.com>
    Tested-by: Dhiren Parmar <dparmar@nvidia.com>
    Reviewed-by: Deepak Nibade <dnibade@nvidia.com>

--- arch/arm/mach-tegra/common.c
+++ arch/arm/mach-tegra/common.c
@@ -199,14 +199,24 @@ EXPORT_SYMBOL(tegra_iram_dev);
 
 static void tegra_update_resize_cfg(phys_addr_t base , size_t size)
 {
-#ifdef CONFIG_TRUSTED_LITTLE_KERNEL
 	int err = 0;
+#ifdef CONFIG_TRUSTED_LITTLE_KERNEL
+#define MAX_RETRIES 6
+	int retries = MAX_RETRIES;
 
+retry:
 	err = gk20a_do_idle();
 	if (!err) {
 		/* Config VPR_BOM/_SIZE in MC */
 		te_set_vpr_params((void *)(uintptr_t)base, size);
 		gk20a_do_unidle();
+	} else {
+		if (retries--) {
+			pr_err("%s:%d: fail retry=%d",
+				__func__, __LINE__, MAX_RETRIES - retries);
+			msleep(1);
+			goto retry;
+		}
 	}
 #endif
 }
