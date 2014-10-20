cec848b2661df6bf3f693f41151f427679ea77d6
Vandana Salve <vsalve@nvidia.com>
2014-10-20 22:02:20 +0530

    dma: coherent: fix VPR dma allocation path
    Fixed VPR allocation path by passing DMA_MEMORY_EXCLUSIVE
    which will only allocate memory from the VPR region.
    Do not allow dma_alloc_coherent() to fall back to
    system memory when it's out of memory in the VPR region
    
    bug 1560078
    bug 1567143
    
    Change-Id: I0752b3a8cd4d97f93fe54f08b1e9a1963d3410b9
    Signed-off-by: Vandana Salve <vsalve@nvidia.com>
    Reviewed-on: http://git-master/r/559471
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>

--- drivers/base/dma-coherent.c
+++ drivers/base/dma-coherent.c
@@ -175,7 +175,7 @@ static int declare_coherent_heap(struct device *dev, phys_addr_t base,
 	dma_set_coherent_mask(dev,  DMA_BIT_MASK(64));
 	err = dma_declare_coherent_memory(dev, 0,
 			base, size,
-			DMA_MEMORY_NOMAP);
+			DMA_MEMORY_NOMAP | DMA_MEMORY_EXCLUSIVE);
 	if (err & DMA_MEMORY_NOMAP) {
 		dev_dbg(dev, "dma coherent mem base (%pa) size (%zu)\n",
 			&base, size);
