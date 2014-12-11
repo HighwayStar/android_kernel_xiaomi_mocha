55338dd9da73b5ca8180c8a9a62fdbdc6b59db4b
Krishna Reddy <vdumpa@nvidia.com>
2014-12-11 11:45:34 -0800

    video: tegra: nvmap: avoid nvmap background fill during low memory
    Avoid NvMap background fill during low memory by checking the
    free memory at right place and time.
    Bug 1580482
    
    Change-Id: Ifa807186a3381b0b1807faffcdce965639076b0b
    Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-on: http://git-master/r/662761
    GVS: Gerrit_Virtual_Submit

--- drivers/video/tegra/nvmap/nvmap_pp.c
+++ drivers/video/tegra/nvmap/nvmap_pp.c
@@ -145,6 +145,7 @@ int __nvmap_page_pool_fill_lots_locked(struct nvmap_page_pool *pool,
 static void nvmap_pp_do_background_fill(struct nvmap_page_pool *pool)
 {
 	int err;
+	struct sysinfo info;
 	u32 pages = 0, nr, i;
 	gfp_t gfp = GFP_NVMAP | __GFP_NOMEMALLOC |
 		    __GFP_NORETRY | __GFP_NO_KSWAPD;
@@ -159,6 +160,9 @@ static void nvmap_pp_do_background_fill(struct nvmap_page_pool *pool)
 		gfp |= __GFP_ZERO;
 
 	do {
+		si_meminfo(&info);
+		if (info.freeram <= (MIN_AVAILABLE_MB << (20 - PAGE_SHIFT)))
+			return;
 		nr = min_t(u32, PENDING_PAGES_SIZE, pages);
 		err = __nvmap_pp_alloc_n_pages(pending_pages, nr, gfp);
 		if (err) {
