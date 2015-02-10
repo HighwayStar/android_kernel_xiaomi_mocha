cbfc0c2c7e547785ca82a71e13b8c9e9c26b90c5
Suresh Mangipudi <smangipudi@nvidia.com>
2015-02-10 16:22:36 +0530

    USB: core: Increase auto-suspend delay for root-hub
    Quick connect and disconnection of USB devices is resulting in the 
    USB device to be not enumerated after a iterations.
    Increasing the USB auto-suspend delay helps in these cases.
    
    Bug 200023873
    
    Change-Id: I4441d67223d930c5ac629bfad82dc68b4a45df7e
    Signed-off-by: Suresh Mangipudi <smangipudi@nvidia.com>
    Reviewed-on: http://git-master/r/691937
    Tested-by: Rohith Seelaboyina <rseelaboyina@nvidia.com>
    Reviewed-by: Rohith Seelaboyina <rseelaboyina@nvidia.com>
    Reviewed-by: Dhiren Parmar <dparmar@nvidia.com>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -1648,7 +1648,7 @@ static int hub_probe(struct usb_interface *intf, const struct usb_device_id *id)
 	desc = intf->cur_altsetting;
 	hdev = interface_to_usbdev(intf);
 
-	pm_runtime_set_autosuspend_delay(&hdev->dev, 200);
+	pm_runtime_set_autosuspend_delay(&hdev->dev, 500);
 
 	/* Hubs have proper suspend/resume support. */
 	usb_enable_autosuspend(hdev);
