5a8aee36545f7c2b49a663751b325dcee119a815
Insei <goodmobiledevices@gmail.com>
2017-01-19 02:42:43 +0300

    kexec-hardboot patch for mocha [Tegra k1]

--- arch/arm/kernel/machine_kexec.c
+++ arch/arm/kernel/machine_kexec.c
@@ -24,6 +24,9 @@ extern unsigned long kexec_start_address;
 extern unsigned long kexec_indirection_page;
 extern unsigned long kexec_mach_type;
 extern unsigned long kexec_boot_atags;
+#ifdef CONFIG_KEXEC_HARDBOOT
+extern unsigned long kexec_hardboot;
+#endif
 
 static atomic_t waiting_for_crash_ipi;
 
@@ -152,6 +155,10 @@ void machine_kexec(struct kimage *image)
 	kexec_mach_type = machine_arch_type;
 	if (!kexec_boot_atags)
 		kexec_boot_atags = image->start - KEXEC_ARM_ZIMAGE_OFFSET + KEXEC_ARM_ATAGS_OFFSET;
+		
+#ifdef CONFIG_KEXEC_HARDBOOT
+	kexec_hardboot = image->hardboot;
+#endif
 
 
 	/* copy our kernel relocation code to the control code page */
